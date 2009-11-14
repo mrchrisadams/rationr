5c23a1b1ef22407c0fdb57f48dc3bdec177aea73
eddd <ed@rationr.com>
2009-11-14 19:32:38 +0000

    added validation and associations

--- app/models/constraint.rb
+++ app/models/constraint.rb
@@ -1,2 +1,11 @@
 class Constraint < ActiveRecord::Base
+
+has_many :comments, :class_name => "comment"
+
+validates_presence_of :title, :on => :create, :message => "can't be blank"
+validates_presence_of :value, :on => :create, :message => "can't be blank"
+validates_numericality_of :value, :on => :create, :message => "must be a number"
+validates_presence_of :unit, :on => :create, :message => "can't be blank"
+validates_presence_of :description, :on => :create, :message => "can't be blank"
+
 end
